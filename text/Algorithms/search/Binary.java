f1d5e5d28ec2df18191eac1f6c639b3292072187
ZohirEDDIG <zohireddig.dev@gmail.com>
2025-10-31 11:34:39 +0100

    Binary Search

---
+++ text/Algorithms/search/Binary.java
@@ -0,0 +1,29 @@
+public class Binary {
+    // Works only if you have a sorted array, else you have to sort the array first
+
+    public static boolean binarySearch(int[] nums, int t) {
+        int leftIndex = 0;
+        int rightIndex = nums.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            int middleIndex = (leftIndex + rightIndex) / 2;
+            int middleValue = nums[middleIndex];
+
+            if (t > middleValue) {
+                leftIndex = middleIndex + 1;
+            } else if (t < middleValue) {
+                rightIndex = middleIndex - 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 7, 9};
+        System.out.println(binarySearch(nums, 5));
+        System.out.println(binarySearch(nums, 8));
+    }
+}
