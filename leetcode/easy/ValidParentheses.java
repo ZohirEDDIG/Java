68e5ac3bf5f1117b58a6ffb1479efe1faeb384e7
ZohirEDDIG <zohireddig.dev@gmail.com>
2025-11-14 23:22:13 +0100

    Update ValidParentheses

--- leetcode/easy/ValidParentheses.java
+++ leetcode/easy/ValidParentheses.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<Character>();
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
